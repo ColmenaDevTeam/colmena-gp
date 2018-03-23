5fa13582d46982a6f510b8c7c064b0f5ab97a99e
Anake TM <anakesoft@gmail.com>
2018-03-23 00:31:19 -0400

    Acomodando esquema de base de datos (bugs every where)

--- database/migrations/2018_03_22_203400_database.php
+++ database/migrations/2018_03_22_203400_database.php
@@ -46,15 +46,12 @@ class Database extends Migration
             $table->increments('id');
             $table->string('title');
             $table->date('estimated_date');
-            $table->date('deliver_date')->nullable();
             $table->text('details');
             $table->integer('priority');
             $table->integer('complexity');
-            $table->enum('status',['Asignada','Revision','Cumplida','Cancelada','Diferida','Retardada'])->default('Asignada');
             $table->enum('type',['Academico-Docente','Administrativas','Creacion intelectual','Integracion Social','Administrativo-Docente','Produccion']);
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->boolean('seen')->default(false);
             $table->timestamps();
         });
         Schema::create('roles', function (Blueprint $table) {
@@ -128,6 +125,58 @@ class Database extends Migration
             $table->increments('id');
             $table->string('uri');
         });
+        Schema::create('commissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('department_id')->unsigned()->nullable();
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
+            $table->string('description');
+        });
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('reason');
+            $table->dateTime('date');
+            $table->string('place');
+            $table->string('agreements');
+            $table->enum('status',['Agendada','Cumplida','Cancelada','Diferida'])->default('Agendada');
+            $table->string('topics');
+            $table->string('observations');
+        });
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('message');
+            $table->boolean('seen');
+            $table->timestamps();
+        });
+        Schema::create('users_has_commissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('commission_id')->unsigned();
+            $table->foreign('commission_id')->references('id')->on('commissions')->onDelete('cascade');
+            $table->boolean('leader');
+        });
+        Schema::create('users_has_meetings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('meeting_id')->unsigned();
+            $table->foreign('meeting_id')->references('id')->on('meetings')->onDelete('cascade');
+            $table->boolean('attendance')->default('false');
+        });
+        Schema::create('users_has_tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('status',['Asignada','Revision','Cumplida','Cancelada','Diferida','Retardada'])->default('Asignada');
+            $table->date('deliver_date')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('task_id')->unsigned();
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+            $table->text('details')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -150,7 +199,12 @@ class Database extends Migration
         if(Schema::hasTable('users_has_recurring_activities')) Schema::drop('users_has_recurring_activities');
         if(Schema::hasTable('calendar')) Schema::drop('calendar');
         if(Schema::hasTable('commissions')) Schema::drop('commissions');
-        if(Schema::hasTable('user_has_commissions')) Schema::drop('user_has_commissions');
+        if(Schema::hasTable('meetings')) Schema::drop('meetings');
+        if(Schema::hasTable('notifications')) Schema::drop('notifications');
         if(Schema::hasTable('report_headers')) Schema::drop('report_headers');
+        if(Schema::hasTable('user_has_commissions')) Schema::drop('user_has_commissions');
+        if(Schema::hasTable('user_has_meetings')) Schema::drop('user_has_meetings');
+        if(Schema::hasTable('user_has_tasks')) Schema::drop('user_has_tasks');
+
     }
 }
