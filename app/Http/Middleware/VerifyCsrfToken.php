8fbabdd0bcd20597e7e3477b2b2c6fae12d5a549
Anake TM <anakesoft@gmail.com>
2018-05-18 10:54:16 -0400

    asda

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use Carbon\Carbon;
+use Symfony\Component\HttpFoundation\Cookie;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
@@ -14,4 +16,27 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    /**
+     * Add the CSRF token to the response cookies.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * Overwritten for add httpOnly flag to Token.
+     */
+    protected function addCookieToResponse($request, $response)
+    {
+        $config = config('session');
+
+        $response->headers->setCookie(
+            new Cookie(
+                'XSRF-TOKEN', $request->session()->token(), Carbon::now()->getTimestamp() + 60 * $config['lifetime'],
+                $config['path'], $config['domain'], $config['secure'], true
+            )
+        );
+
+        return $response;
+    }
 }
